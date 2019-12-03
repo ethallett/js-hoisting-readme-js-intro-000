c596aab5cdb2d1008a52305df600c84169fced15
Elizabeth T Hallett <ethallett@gmail.com>
2019-12-03 01:13:31 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,20 +1,21 @@
 function callMe() {
   var lyric = "maybe";
+  return(lyric);
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return(lyric);
+
 }
-callMe()
+
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
